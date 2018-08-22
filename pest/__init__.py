2103e660552ba6b0a5e3bd433ff8ec434f919e39
Ben Evans <ben.d.evans@gmail.com>
2018-08-22 14:46:38 +0100

    Add basic __init__.py

---
+++ pest/__init__.py
@@ -0,0 +1,8 @@
+"""Protein Evolution with Sequence Tracking."""
+
+__doc__ = """A Python module for evolving proteins."""
+# Main module file for PyRhO
+
+__all__ = ["pest", "test_normal_distribution"]
+__project__ = "pest"
+__version__ = "1.0"

5fb1fea9dcaf1eb551e19259578d2e2f44871161
Ben Evans <ben.d.evans@gmail.com>
2018-10-10 18:05:33 +0100

    Add compact_protein

--- pest/utilities.py
+++ pest/utilities.py
@@ -1,4 +1,8 @@
+def compact_protein(protein):
+    """Takes a list of amino acids and returns them as a string."""
+    return ''.join(protein)
+
 
 def print_protein(protein):
     """Takes a list of amino acids and prints them as a string."""
-    print(''.join(protein))
+    print(compact_protein(protein))

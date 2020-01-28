c4bd32afa2753b884b7ecbfe750bbae1847b4c59
Ben Evans <ben.d.evans@gmail.com>
2020-01-28 18:07:13 +0000

    Remove cruft

--- pest/__init__.py
+++ pest/__init__.py
@@ -1,8 +1,6 @@
 """Protein Evolution with Sequence Tracking."""
 
 __doc__ = """A Python module for evolving proteins."""
-# Main module file for PyRhO
-
 __all__ = ["pest"]
 __project__ = "pest"
 __version__ = "1.0"

f97a9e4aa9bf02b570cecc2c21d18180a0e24a70
Benjamin <ben.d.evans@gmail.com>
2023-01-17 12:02:12 +0000

    Add docstring and remove cruft

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ import os
 from setuptools import setup, find_packages
 import codecs
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-# def read(fname):
-#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*parts):
+    """Utility function to read the README file for the long description.
+
+    This avoids duplication by reading the top-level README file.
+    It is also easier to edit that than put a raw string below.
+    """
     with codecs.open(os.path.join(here, *parts), 'r') as fp:
         return fp.read()
 

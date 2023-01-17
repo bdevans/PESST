5618471be556c966d1c2a07b53915db4c70fe0ed
Benjamin <ben.d.evans@gmail.com>
2023-01-17 12:02:58 +0000

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -50,9 +50,11 @@ setup_kwargs = {
                     "Topic :: Scientific/Engineering :: Artificial Life",
                     "Topic :: Scientific/Engineering :: Bio-Informatics",
                     "Programming Language :: Python :: 3 :: Only",
-                    "Programming Language :: Python :: 3.6",
                     "Programming Language :: Python :: 3.7",
                     "Programming Language :: Python :: 3.8",
+                    "Programming Language :: Python :: 3.9",
+                    "Programming Language :: Python :: 3.10",
+                    "Programming Language :: Python :: 3.11",
                     "License :: OSI Approved :: BSD License"],
 }
 
